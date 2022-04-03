dd197019f3e3f442d7144399a9c3a5c6fd1f9b14
Boris Gusev <57191011+999Newone999@users.noreply.github.com>
2022-04-03 13:02:16 +0300

    Add files via upload

---
+++ Gusev_ITMO_Lab16_dll/ProductClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+
+namespace Gusev_ITMO_Lab16_dll
+{
+    public class Product
+    {
+        [JsonPropertyName("Код товара")]
+        public int ProductCode { get; set; }
+        [JsonPropertyName("Название товара")]
+        public string ProductName { get; set; }
+        [JsonPropertyName("Цена товара")]
+        public double ProductPrice { get; set; }
+    }
+}
